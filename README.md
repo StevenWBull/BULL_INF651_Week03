1c8059033bad3ecc3d31d92f9ee6c5f930b3f18e
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-06-06 14:40:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+ASSIGNMENT 2 for INF651 on Week 3:
+
+Completed by Steven Bull on 06/06/2023.
+
+For ease of use I have included console logging. This can easily be ran with node.
+
+node main.js
---
+++ main.js
@@ -0,0 +1,40 @@
+// Question 1:
+let myNumber;
+console.log(`Question 1: myNumber is declared and is currently of type ${typeof myNumber}`);
+
+// Question 2:
+myNumber = 5;
+console.log(`Question 2: myNumber is ${myNumber} and is a ${typeof myNumber}`);
+
+// Question 3:
+let myOtherNumber = 5;
+console.log(`Question 3: myOtherNumber is ${myOtherNumber} and is a ${typeof myOtherNumber}`);
+
+// Question 4:
+let myTotal = myNumber * myOtherNumber;
+console.log(`Question 4: myTotal is ${myTotal} and is a ${typeof myTotal}`);
+
+// Question 5:
+let myName = "Steven";
+let myCombo = myNumber + myOtherNumber;
+console.log(`Question 5: myName is ${myName} and is a ${typeof myName}, myCombo is ${myCombo} and is a ${typeof myCombo}`);
+
+// Question 6:
+let myOtherCombo = myNumber + myOtherNumber;
+console.log(`Question 6: myOtherCombo is ${myOtherCombo} and is a ${typeof myOtherCombo}`);
+
+// Question 7:
+let myRemainder = myNumber % 5;
+console.log(`Question 7: myRemainder is ${myRemainder} and is a ${typeof myRemainder}`);
+
+// Question 8:
+let i = 2;
+console.log(`Question 8: i is ${i} and is a ${typeof i}`);
+
+// Question 9:
+let int = 4;
+console.log(`Question 9: int is ${int} and is a ${typeof int}`);
+
+// Question 10:
+let myBoolean = false;
+console.log(`Question 10: myBoolean is ${myBoolean} and is a ${typeof myBoolean}`);
